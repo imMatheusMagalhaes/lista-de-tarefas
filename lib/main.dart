c9954e148f8547c641a0f5fb823f72d9a53151d1
imMatheusMagalhaes <magalhaes.almeida.matheus@gmail.com>
2020-01-27 14:45:33 -0300

    layout

--- lib/main.dart
+++ lib/main.dart
@@ -6,9 +6,7 @@ import 'dart:async';
 void main() => runApp(MaterialApp(
     title: 'Lista de Tarefas',
     home: Home(),
-    theme: ThemeData(
-      primaryColor: Colors.green,
-    )));
+    theme: ThemeData(primaryColor: Colors.blue)));
 
 class Home extends StatefulWidget {
   @override
@@ -19,7 +17,9 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        backgroundColor: Color.fromRGBO(21, 32, 43, 10),
         appBar: AppBar(
+          backgroundColor: Colors.blue,
           title: Padding(
             padding: EdgeInsets.all(100),
             child: Text('Lista de Tarefas', style: TextStyle(fontSize: 25)),
@@ -29,17 +29,25 @@ class _HomeState extends State<Home> {
           padding: EdgeInsets.all(8.0),
           child: Row(
             children: <Widget>[
-              Expanded(flex: 3, 
-                child: TextField(
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                  ),
-                )),
+              Expanded(
+                  flex: 3,
+                  child: Padding(
+                    padding: const EdgeInsets.all(5.0),
+                    child: TextField(
+                      decoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                          focusedBorder: OutlineInputBorder(
+                              borderSide: BorderSide(color: Colors.blue)),
+                          enabledBorder: OutlineInputBorder(
+                              borderSide: BorderSide(color: Colors.blue))),
+                      style: TextStyle(fontSize: 20, color: Colors.blue)
+                    ),
+                  )),
               Expanded(
                 child: ButtonTheme(
-                  height: 60,
+                  height: 63,
                   child: RaisedButton(
-                    color: Colors.green,
+                    color: Colors.blue,
                     child: Text('Add',
                         style: TextStyle(fontSize: 20, color: Colors.white)),
                     onPressed: () {},
