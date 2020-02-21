3b2f2cf00edc6a41ed3a41d1943f4b8dd5010eee
imMatheusMagalhaes <magalhaes.almeida.matheus@gmail.com>
2020-02-21 14:44:26 -0300

    projeto pronto

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+import 'dart:ffi';
 import 'package:flutter/material.dart';
 import 'package:path_provider/path_provider.dart';
 import 'dart:io';
@@ -6,7 +8,7 @@ import 'dart:async';
 void main() => runApp(MaterialApp(
     title: 'Lista de Tarefas',
     home: Home(),
-    theme: ThemeData(primaryColor: Colors.blue)));
+    theme: ThemeData(primaryColor: Colors.blueAccent)));
 
 class Home extends StatefulWidget {
   @override
@@ -14,53 +16,176 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  final _toDoController = TextEditingController();
+
+  List _toDoList = [];
+
+  Map<String, dynamic> _lastRemoved;
+  int _lastRemovedPos;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _readData().then((data) {
+      setState(() {
+        _toDoList = json.decode(data);
+      });
+    });
+  }
+
+  void _addToDo() {
+    setState(() {
+      Map<String, dynamic> newToDo = Map();
+      newToDo["title"] = _toDoController.text;
+      _toDoController.clear();
+      newToDo["ok"] = false;
+      _toDoList.add(newToDo);
+      _saveData();
+    });
+  }
+
+  Future<Null> _refresh() async{
+    await Future.delayed(Duration(seconds: 1));
+
+    setState(() {
+      _toDoList.sort((a, b){
+      if(a["ok"] && !b["ok"]) return 1;
+      else if(!a["ok"] && b["ok"]) return -1;
+      else return 0; 
+
+    });
+
+      _saveData();
+    });
+
+    return null;
+
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Color.fromRGBO(21, 32, 43, 10),
         appBar: AppBar(
-          backgroundColor: Colors.blue,
+          backgroundColor: Colors.blueAccent,
           title: Padding(
             padding: EdgeInsets.all(100),
             child: Text('Lista de Tarefas', style: TextStyle(fontSize: 25)),
           ),
         ),
-        body: Container(
-          padding: EdgeInsets.all(8.0),
-          child: Row(
-            children: <Widget>[
-              Expanded(
-                  flex: 3,
-                  child: Padding(
-                    padding: const EdgeInsets.all(5.0),
-                    child: TextField(
-                      decoration: InputDecoration(
-                          border: OutlineInputBorder(),
-                          focusedBorder: OutlineInputBorder(
-                              borderSide: BorderSide(color: Colors.blue)),
-                          enabledBorder: OutlineInputBorder(
-                              borderSide: BorderSide(color: Colors.blue))),
-                      style: TextStyle(fontSize: 20, color: Colors.blue)
+        body: Column(
+          children: <Widget>[
+            Container(
+              padding: EdgeInsets.all(8.0),
+              child: Row(
+                children: <Widget>[
+                  Expanded(
+                      flex: 3,
+                      child: Padding(
+                        padding: const EdgeInsets.all(5.0),
+                        child: TextField(
+                            controller: _toDoController,
+                            decoration: InputDecoration(
+                                labelText: "Nova Tarefa",
+                                labelStyle: TextStyle(color: Colors.blueAccent),
+                                border: OutlineInputBorder(),
+                                focusedBorder: OutlineInputBorder(
+                                    borderSide:
+                                        BorderSide(color: Colors.blueAccent)),
+                                enabledBorder: OutlineInputBorder(
+                                    borderSide:
+                                        BorderSide(color: Colors.blueAccent))),
+                            style: TextStyle(
+                                fontSize: 20, color: Colors.blueAccent)),
+                      )),
+                  ButtonTheme(
+                    height: 63,
+                    child: RaisedButton(
+                      color: Colors.blueAccent,
+                      child: Text('Add',
+                          style: TextStyle(fontSize: 20, color: Colors.white)),
+                      onPressed: _addToDo,
                     ),
-                  )),
-              Expanded(
-                child: ButtonTheme(
-                  height: 63,
-                  child: RaisedButton(
-                    color: Colors.blue,
-                    child: Text('Add',
-                        style: TextStyle(fontSize: 20, color: Colors.white)),
-                    onPressed: () {},
-                  ),
-                ),
+                  )
+                ],
+              ),
+            ),
+            Expanded(
+              child: RefreshIndicator(onRefresh: _refresh,
+              child: ListView.builder(
+                  padding: EdgeInsets.only(top: 10.0),
+                  itemCount: _toDoList.length,
+                  itemBuilder: buildItem),
               )
-            ],
-          ),
+            ),
+          ],
         ));
   }
-}
 
-Future<File> _getFile() async {
-  final diretorio = await getApplicationDocumentsDirectory();
-  return File("${diretorio}");
+  Widget buildItem(context, index) {
+    return Dismissible(
+      key: Key(DateTime.now().millisecondsSinceEpoch.toString()),
+      background: Container(
+          color: Colors.red,
+          child: Align(
+            alignment: Alignment(-0.9, 0.0),
+            child: Icon(Icons.delete, color: Colors.white),
+          )),
+      direction: DismissDirection.startToEnd,
+      child: CheckboxListTile(
+        title: Text(_toDoList[index]["title"] ?? ""),
+        value: _toDoList[index]["ok"],
+        secondary: CircleAvatar(
+            child: Icon(_toDoList[index]["ok"] ? Icons.check : Icons.error)),
+        onChanged: (c) {
+          setState(() {
+            _toDoList[index]["ok"] = c;
+            _saveData();
+          });
+        },
+      ),
+      onDismissed: (direction){
+        _lastRemoved = Map.from(_toDoList[index]);
+        _lastRemovedPos = index;
+        _toDoList.removeAt(index);
+
+        _saveData();
+
+        final snack = SnackBar(
+          content: Text("Tarefa ${_lastRemoved["title"]} removida!"),
+          action: SnackBarAction(label: "Desfazer", 
+          onPressed: (){
+            setState(() {
+              _toDoList.insert(_lastRemovedPos, _lastRemoved);
+              _saveData();
+            });
+          }
+          ),
+          duration: Duration(seconds: 2),
+        );
+        Scaffold.of(context).removeCurrentSnackBar();
+        Scaffold.of(context).showSnackBar(snack);
+      },
+    );
+  }
+
+  Future<File> _getFile() async {
+    final directory = await getApplicationDocumentsDirectory();
+    return File("${directory.path}/data.json");
+  }
+
+  Future<File> _saveData() async {
+    String data = json.encode(_toDoList);
+    final file = await _getFile();
+    return file.writeAsString(data);
+  }
+
+  Future<String> _readData() async {
+    try {
+      final file = await _getFile();
+      return file.readAsString();
+    } catch (e) {
+      return null;
+    }
+  }
 }
